e5118e3e7d0f36233083d97ecec5640f90a52e2b
vinayak9775 <vinayak@sperohealthcare.in>
2025-09-05 16:37:11 +0530

    updated

--- DMS_client/src/Componenets/Footer/Footer.jsx
+++ DMS_client/src/Componenets/Footer/Footer.jsx
@@ -1,53 +1,11 @@
-<<<<<<< HEAD
-import { Box, Typography } from '@mui/material';
-import logo from '../../assets/spero_copy_right.png';
-=======
 import { Box, Typography } from "@mui/material";
 import logo from "../../assets/spero_copy_right.png";
 import Efkon from "../../assets/image (48).png";
->>>>>>> Development
 
 export default function Footer({ darkMode }) {
   return (
     <Box
       sx={{
-<<<<<<< HEAD
-        position: 'fixed',
-        bottom: 0,
-        height: '30px',
-        width: '100%',
-        zIndex: 1000,
-        backgroundColor: darkMode ? "#202328" : "#CCDBEF",
-        transition: "background-color 0.5s ease-in-out, color 0.5s ease-in-out",
-        color: darkMode ? 'white' : 'black',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',  // center horizontally
-        px: 2,
-        gap: 1,  // gap between text and image
-      }}
-    >
-      <Typography
-        variant="subtitle2"
-        sx={{
-          fontFamily: 'sans-serif',
-          fontStyle: 'normal',
-          textDecoration: 'none',
-        }}
-      >
-        Powered by
-      </Typography>
-
-      <Box
-        component="img"
-        src={logo}
-        alt="Logo"
-        sx={{
-          height: 30,
-          width: 50,
-        }}
-      />
-=======
         position: "fixed",
         bottom: 0,
         height: "40px",
@@ -93,7 +51,6 @@ export default function Footer({ darkMode }) {
           }}
         />
       </Box>
->>>>>>> Development
     </Box>
   );
 }
